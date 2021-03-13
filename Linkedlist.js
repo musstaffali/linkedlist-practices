1e9161461f8d7323e7162d020df8efa86a0b45a6
Mustafa Mustafa <mamustafa@gtcc.edu>
2021-03-12 20:53:23 -0500

    .js

---
+++ Linkedlist.js
@@ -0,0 +1,22 @@
+class LinkedList {
+    constructor() {
+        this.head = null
+        this.length = 0
+    }
+    insertAtHead(data) {
+        // created a new node // gave it the data value we wanted to store // next value after element is start of list 
+        const newNode = new LinkedListNode(data, this.head)
+        // replace start of list with new node
+        this.head = newNode
+        length++
+    }
+}
+
+class LinkedListNode {
+    constructor(value, next) {
+        this.value = value
+        this.next = next
+    }
+}
+
+module.exports = LinkedList
